c8798c0e557043f1f1c31d6fea9fca9b28fe4f76
yanghq <1040926235@qq.com>
2025-05-26 11:19:16 +0800

    1.0.1

--- spring-boot3-examples/examples-workflow/src/test/java/io/github/guoshiqiufeng/dify/examples/workflow/WorkflowTest.java
+++ spring-boot3-examples/examples-workflow/src/test/java/io/github/guoshiqiufeng/dify/examples/workflow/WorkflowTest.java
@@ -1,6 +1,8 @@
 package io.github.guoshiqiufeng.dify.examples.workflow;
 
 import com.alibaba.fastjson2.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import io.github.guoshiqiufeng.dify.core.pojo.DifyPageResult;
 import io.github.guoshiqiufeng.dify.workflow.DifyWorkflow;
 import io.github.guoshiqiufeng.dify.workflow.dto.request.WorkflowLogsRequest;
@@ -9,13 +11,16 @@ import io.github.guoshiqiufeng.dify.workflow.dto.response.WorkflowInfoResponse;
 import io.github.guoshiqiufeng.dify.workflow.dto.response.WorkflowLogs;
 import io.github.guoshiqiufeng.dify.workflow.dto.response.WorkflowRunResponse;
 import io.github.guoshiqiufeng.dify.workflow.dto.response.WorkflowRunStreamResponse;
+import io.github.guoshiqiufeng.dify.workflow.enums.StreamEventEnum;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import reactor.core.publisher.Flux;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -33,6 +38,8 @@ public class WorkflowTest {
     @Resource
     private DifyWorkflow difyWorkflow;
 
+    @Resource
+    private ObjectMapper objectMapper;
 
     @Test
     public void test() throws InterruptedException {
@@ -62,5 +69,28 @@ public class WorkflowTest {
         log.info("logs: {}", JSON.toJSONString(logs));
 
         Thread.sleep(20000);
+    }
+
+    @Test
+    public void testDeserializeTextChunkDataNewline() throws IOException {
+        // Prepare test JSON
+        String json = "{\n" +
+                "    \"event\": \"text_chunk\",\n" +
+                "    \"workflow_run_id\": \"wfr-123456\",\n" +
+                "    \"task_id\": \"task-123456\",\n" +
+                "    \"data\": {\n" +
+                "        \"text\": \"\\n\\n\",\n" +
+                "        \"from_variable_selector\": [\n" +
+                "        ]\n" +
+                "    }\n" +
+                "}";
+        objectMapper.disable(SerializationFeature.INDENT_OUTPUT);
+        // Deserialize JSON
+        WorkflowRunStreamResponse response = objectMapper.readValue(json, WorkflowRunStreamResponse.class);
+
+        // Assertions
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(StreamEventEnum.text_chunk, response.getEvent());
+
     }
 }
