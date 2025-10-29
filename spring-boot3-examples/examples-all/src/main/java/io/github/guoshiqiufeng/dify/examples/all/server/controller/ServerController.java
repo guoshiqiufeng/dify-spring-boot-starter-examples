a5970a68c2eb6b5ece3ab563877a458e4008fbc1
yanghq <1040926235@qq.com>
2025-10-29 13:56:43 +0800

    1.4.6

---
+++ spring-boot3-examples/examples-all/src/main/java/io/github/guoshiqiufeng/dify/examples/all/server/controller/ServerController.java
@@ -0,0 +1,29 @@
+package io.github.guoshiqiufeng.dify.examples.all.server.controller;
+
+import io.github.guoshiqiufeng.dify.server.DifyServer;
+import io.github.guoshiqiufeng.dify.server.dto.response.AppsResponse;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author yanghq
+ * @version 1.0
+ * @since 2025/10/28 11:09
+ */
+@RestController
+@RequestMapping("/server")
+public class ServerController {
+
+    @Resource
+    private DifyServer difyServer;
+
+    @GetMapping("/apps")
+    public List<AppsResponse> apps() {
+        return difyServer.apps("", "");
+    }
+
+}
